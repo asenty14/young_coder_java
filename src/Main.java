dd044768320331ac705455056fe59693c0467ea1
DLC-2 <alexeylichman1@gmail.com>
2024-12-03 17:44:54 +0200

    made rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 
+import figures.Rectangle;
+
+
 public class Main {
+
+
     public static void main(String[] args) {
 
+
+        Rectangle square = new Rectangle(8, 4);
+
+        System.out.println(square.);
+
+    }
+
+    public Main() {
+
+
+        System.out.println();
     }
 }
+
+
+
+
+
 
---
+++ src/figures/Rectangle.java
@@ -0,0 +1,20 @@
+package figures;
+
+public class Rectangle {
+    int a;
+    int b;
+
+    public Rectangle(int storona1, int storona2) {
+        a = storona1;
+        b = storona2;
+    }
+
+    public getArea(String[] args) {
+
+        Rectangle square = new Rectangle(8, 4);
+        square.a = 8;
+        square.b = 4;
+        return square;
+
+    }
+}
