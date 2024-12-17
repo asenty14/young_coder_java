c769fda8e8e2437cb3fe5a26077ffa1f431282ba
DLC-2 <alexeylichman1@gmail.com>
2024-12-17 17:23:44 +0200

    new method

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-
-import figures.Circle;
-import figures.Rectangle;
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle1 = new Rectangle(8, 4);
-        int area = rectangle1.getArea();
-        System.out.println("Площа прямокутника = " + area);
+        //Rectangle rectangle1 = new Rectangle(8, 4);
+        //int area = rectangle1.getArea();
+        //System.out.println("Площа прямокутника = " + area);
 
-        Rectangle rectangle2 = new Rectangle(9, 4);
-        int perimeter = rectangle2.getPerimeter();
-        System.out.println("Периметр прямокутника = " + perimeter);
+        //Rectangle rectangle2 = new Rectangle(9, 4);
+        //int perimeter = rectangle2.getPerimeter();
+        //System.out.println("Периметр прямокутника = " + perimeter);
 
-        Circle circle1 = new Circle(8);
-        double circumference = circle1.getCircumference();
-        System.out.println("Довжина окружності кола = " + circumference);
+        //Circle circle1 = new Circle(8);
+        //double circumference = circle1.getCircumference();
+        //System.out.println("Довжина окружності кола = " + circumference);
 
-        Circle circle2 = new Circle(9);
-        double areaofacircle = circle2.getCircumference();
-        System.out.println("Площа кола = " + areaofacircle);
+        //Circle circle2 = new Circle(9);
+        //double areaofacircle = circle2.getCircumference();
+        // System.out.println("Площа кола = " + areaofacircle);
+        myChefir();
     }
+
+    static void myChefir() {
+        String x = "java " + "code";
+        int y = 744 * 2;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
 }
 
 
